a07e03ee3fa9ce747a1c3175fcc3295e37666618
Borislava Kirova <b.kirova@dopamine.bg>
2021-06-10 09:39:59 +0300

    feat: add components

--- pages/trends.js
+++ pages/trends.js
@@ -1,12 +1,13 @@
 import Layout from '../components/layout'
 import Dashboard from '../components/dashboard'
+import Timeline from '../components/timeline'
 import Card from '../components/card'
 
 export default function Trends() {
     return (
       <Layout home>
+          <br/><br/><br/><br/><br/><br/>
           <Card/>
-        <Dashboard/>
       </Layout>
     )
   }
