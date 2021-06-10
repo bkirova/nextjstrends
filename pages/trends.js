b938d0e801c4f1875455a520df114da9455461d4
Borislava Kirova <b.kirova@dopamine.bg>
2021-06-10 09:58:15 +0300

    fix: remove example files

--- pages/trends.js
+++ pages/trends.js
@@ -1,6 +1,4 @@
 import Layout from '../components/layout'
-import Dashboard from '../components/dashboard'
-import Timeline from '../components/timeline'
 import Card from '../components/card'
 
 export default function Trends() {
