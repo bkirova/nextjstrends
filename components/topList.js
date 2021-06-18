b9fbf5d6212d69d0c07f535dff10ee40e4d8b697
Borislava Kirova <b.kirova@dopamine.bg>
2021-06-18 18:00:59 +0300

    fix: styles

--- components/topList.js
+++ components/topList.js
@@ -2,7 +2,7 @@ import Card from './card'
 
 export default function TopList(props) {
     return (
-    <div class="pt-20">
+    <div class="">
           
         <div class="min-w-full">
             <div class="grid grid-cols-1 gap-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
