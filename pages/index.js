b49bb6eb54edb3c9d8d02967d0543ec74d4accad
Borislava Kirova <b.kirova999@gmail.com>
2021-07-10 11:26:05 +0300

    fix: add home pic

--- pages/index.js
+++ pages/index.js
@@ -34,14 +34,14 @@ export default function Home() {
             <div className="w-full mx-auto mt-20 text-center md:w-10/12">
                 <div className="relative z-0 w-full mt-8">
                     <div className="relative overflow-hidden shadow-2xl">
-                        <div className="flex items-center flex-none px-4 bg-green-400 rounded-b-none h-11 rounded-xl">
+                        <div className="flex items-center flex-none px-4 bg-blue-300 rounded-b-none h-11 rounded-xl">
                             <div className="flex space-x-1.5">
                                 <div className="w-3 h-3 border-2 border-white rounded-full"></div>
                                 <div className="w-3 h-3 border-2 border-white rounded-full"></div>
                                 <div className="w-3 h-3 border-2 border-white rounded-full"></div>
                             </div>
                         </div>
-                        <img src="https://cdn.devdojo.com/images/march2021/green-dashboard.jpg"/>
+                        <img className="opacity-60" src="/images/posts.jpg"/>
                     </div>
                 </div>
             </div>
