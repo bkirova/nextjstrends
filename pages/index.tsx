fc912eb19214363f5d53cdeae01319d3b5534c36
Borislava Kirova <b.kirova999@gmail.com>
2021-07-18 19:00:37 +0300

    fix: styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,14 @@ function Home({ items }) {
                 duration: 0.5,
             }}
             className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-                <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+                <h1 className="mb-8 text-4xl font-extrabold text-gray-900 md:text-6xl md:tracking-tight">
                     <span>Start</span> <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">building your content</span> <span>with AI 🤖</span>
                 </h1>
                 <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
                     Start gaining the traction you've always wanted with our next-level templates and designs. Crafted to help you tell your story.
                 </p>
                 <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                    <a href="/generated" className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-purple-400 hover:bg-purple-300 rounded-2xl sm:w-auto sm:mb-0">
+                    <a href="/generated" className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 shadow-lg text-lg text-white bg-purple-400 hover:bg-purple-300 rounded-2xl sm:w-auto sm:mb-0">
                         Get Started
                         <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
