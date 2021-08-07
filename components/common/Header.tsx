a224b2786345c24e61bc2ce3b5012266b3dea8c2
Borislava Kirova <b.kirova999@gmail.com>
2021-08-07 09:17:02 +0300

    fix: header style

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -42,7 +42,7 @@ export default function Menu() {
                   </li> */}
                   <li className="nav-item">
                     <a
-                      className="px-3 py-2 text-base font-medium text-gray-700 hover:text-gray-900 items-center inline-flex"
+                      className="px-3 py-2 text-base font-medium text-gray-500 hover:text-gray-900 items-center inline-flex"
                       href="/generated"
                     >
                       AI
